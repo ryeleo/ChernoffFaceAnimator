84bf5438ff1bedcfd3e17ede798aefa69bad77b1
pem <pem@cs.uoregon.edu>
2015-11-03 13:21:17 -0800

    started adding listeners to view.

--- src/mayapy/views/assignment3/Assignment3Widget.py
+++ src/mayapy/views/assignment3/Assignment3Widget.py
@@ -9,34 +9,46 @@ from pyglass.widgets.PyGlassWidget import PyGlassWidget
 #___________________________________________________________________________________________________ Assignment3Widget
 class Assignment3Widget(PyGlassWidget):
     """A class for Assignment 1"""
+	
+	#===================================================================================================
+	#                                                                                       C L A S S
+	
+	
+	#___________________________________________________________________________________________________ __variables__
+	currTime
+	
 
-#===================================================================================================
-#                                                                                       C L A S S
-
-#___________________________________________________________________________________________________ __init__
-    def __init__(self, parent, **kwargs):
+	#___________________________________________________________________________________________________ __init__
+	def __init__(self, parent, **kwargs):
         """Creates a new instance of Assignment3Widget."""
         super(Assignment3Widget, self).__init__(parent, **kwargs)
         self.runBtn.clicked.connect(self._handleRunButton)
         self.homeBtn.clicked.connect(self._handleReturnHome)
-
-#===================================================================================================
-#                                                                                 H A N D L E R S
-
-#___________________________________________________________________________________________________ _handleReturnHome
-    def _handleRunButton(self):
-        """
-        This callback creates a polygonal cylinder in the Maya scene.
-
-        """
-        r = 50
-        a = 2.0*r
-        y = (0, 1, 0)
-        c = cmds.polyCylinder(
-            r=r, h=5, sx=40, sy=1, sz=1, ax=y, rcp=0, cuv=2, ch=1, n='exampleCylinder2')[0]
-        cmds.select(c)
-        response = nimble.createRemoteResponse(globals())
-        response.put('name', c)
-#___________________________________________________________________________________________________ _handleReturnHome
-    def _handleReturnHome(self):
-        self.mainWindow.setActiveWidget('home')
+		self.timeBox.valueChanged[PySide.QtCore.int].connect(self._handleTimeChange)
+	#===================================================================================================
+	#                                                                                 H A N D L E R S
+	
+	#___________________________________________________________________________________________________ _handleReturnHome
+	def _handleRunButton(self):
+		"""
+		This callback creates a polygonal cylinder in the Maya scene.
+		
+		"""
+		time = self.currTime
+		r = 50
+		a = 2.0*r
+		y = (0, 1, 0)
+		c = cmds.polyCylinder(
+			r=r, h=5, sx=40, sy=1, sz=1, ax=y, rcp=0, cuv=2, ch=1, n='exampleCylinder2')[0]
+		cmds.select(c)
+		response = nimble.createRemoteResponse(globals())
+		response.put('name', c)
+		#___________________________________________________________________________________________________ _handleReturnHome
+	def _handleReturnHome(self):
+		self.mainWindow.setActiveWidget('home')
+		
+		
+	#___________________________________________________________________________________________________ _handleReturnHome
+    def _handleTimeChange(self, arg_1):
+		self.currTime = arg_1
+		self.
